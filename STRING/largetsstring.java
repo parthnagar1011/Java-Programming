e196fdcf4a8176feb6503d70639ac432c8b4d0ce
Parth Nagar <149687711+parthnagar1011@users.noreply.github.com>
2025-07-14 01:59:11 +0530

    Add files via upload

---
+++ STRING/largetsstring.java
@@ -0,0 +1,13 @@
+public class largetsstring {
+    public static void main(String[] args) {
+        String fruits[] = {"Apple" , "Mango" ,"Banana"};
+        String largest = fruits[0];
+
+        for(int i = 1; i<fruits.length;i++) {
+            if(largest.compareTo(fruits[i]) <= 0) {
+                largest = fruits[i];
+            }
+        }
+        System.out.println(largest);
+    }
+}
